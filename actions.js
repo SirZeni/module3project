ab1846857a317226b59e298ce248643c74fee77e
SirZeni <115522316+SirZeni@users.noreply.github.com>
2023-06-23 13:04:52 -0500

    Add files via upload

---
+++ actions.js
@@ -0,0 +1,9 @@
+function myFunction(){
+document.getElementById("list1").innerHTML = "Chicken";
+document.getElementById("list2").innerHTML = "Beef";
+document.getElementById("list3").innerHTML = "Sushi";
+document.getElementById("listbox");
+document.getElementById("chickenlink");
+document.getElementById("beeflink");
+document.getElementById("sushilink");
+}
